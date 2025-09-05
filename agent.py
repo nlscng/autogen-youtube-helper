73458ad6bf41bdabcffbbe0d31865c597d441cce
ncheng <nlscng.g@gmail.com>
2025-09-04 22:14:48 -0500

    Add another func to get transcript with timestamp.

--- agent.py
+++ agent.py
@@ -11,13 +11,24 @@ async def get_video_transcript(url: str) -> str:
     title: str = yt.title
     video_id: str = url.split('v=')[1].split('&')[0]
     yt_transcript_fetcher: YouTubeTranscriptApi = YouTubeTranscriptApi()
-    feteched_transcript: FetchedTranscript = yt_transcript_fetcher.fetch(video_id, ['en'])
-    transcript_text: str = ' '.join([one_snipet.text for one_snipet in feteched_transcript.snippets])
+    fetched_transcript: FetchedTranscript = yt_transcript_fetcher.fetch(video_id, ['en'])
+    transcript_text: str = ' '.join([one_snipet.text for one_snipet in fetched_transcript.snippets])
     result = f'Title: {title}\n\nDescription: {description}\n\nTranscript: {transcript_text}'
     return result
 
+async def get_video_transcript_with_timestamp(url: str) -> str:
+    """Given a url of type str, return the title, description, and transcript of the video in a str"""
+    yt = YouTube(url)
+    description: str = yt.description
+    title: str = yt.title
+    video_id: str = url.split('v=')[1].split('&')[0]
+    yt_transcript_fetcher: YouTubeTranscriptApi = YouTubeTranscriptApi()
+    fetched_transcript: FetchedTranscript = yt_transcript_fetcher.fetch(video_id, languages=['en'])
+    transcript_text: str = ', '.join([str(one_snipet.start) + ":" + one_snipet.text for one_snipet in fetched_transcript.snippets])
+    result = f'Title: {title}\n\nDescription: {description}\n\nTranscript with timestamp: {transcript_text}'
+    return result
 
 if __name__ == "__main__":
     url = 'https://www.youtube.com/watch?v=9_PepvnqIfU&t=461s'
-    result = asyncio.run(get_video_transcript(url))
+    result = asyncio.run(get_video_transcript_with_timestamp(url))
     print(result)
