5bf4accb53ac7eff9c035ffa887d20932d3bb243
ncheng <nlscng.g@gmail.com>
2025-09-04 21:55:34 -0500

    Refactor into a method that gets title, desc, transcript.

--- agent.py
+++ agent.py
@@ -1,20 +1,22 @@
 from pytubefix import YouTube
 from youtube_transcript_api import YouTubeTranscriptApi, FetchedTranscript
 from typing import List
-
-url = 'https://www.youtube.com/watch?v=9_PepvnqIfU&t=461s'
-
-yt = YouTube(url)
-
-print(yt.title)
-print(yt.description)
+import asyncio
 
 
-video_id: str = url.split('v=')[1].split('&')[0]
-yt_transcript_fetcher: YouTubeTranscriptApi = YouTubeTranscriptApi()
-res: FetchedTranscript = yt_transcript_fetcher.fetch(video_id, ['en'])
+async def get_video_transcript(url: str) -> str:
+    yt = YouTube(url)
+    description: str = yt.description
+    title: str = yt.title
+    video_id: str = url.split('v=')[1].split('&')[0]
+    yt_transcript_fetcher: YouTubeTranscriptApi = YouTubeTranscriptApi()
+    feteched_transcript: FetchedTranscript = yt_transcript_fetcher.fetch(video_id, ['en'])
+    transcript_text: str = ' '.join([one_snipet.text for one_snipet in feteched_transcript.snippets])
+    result = f'Title: {title}\n\nDescription: {description}\n\nTranscript: {transcript_text}'
+    return result
 
-print(res)
 
-transcript_text: str = ' '.join([item['text'] for item in res])
-
+if __name__ == "__main__":
+    url = 'https://www.youtube.com/watch?v=9_PepvnqIfU&t=461s'
+    result = asyncio.run(get_video_transcript(url))
+    print(result)
