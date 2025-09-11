8fc791b265348097235453191c7cf8f86fe4c9e0
ncheng <nlscng.g@gmail.com>
2025-09-10 22:11:48 -0500

    Update model temp, reflect on tool use, etc.

--- agent.py
+++ agent.py
@@ -34,11 +34,11 @@ async def get_video_transcript_with_timestamp(url: str) -> str:
     result = f'Title: {title}\n\nDescription: {description}\n\nTranscript with timestamp: {transcript_text}'
     return result
 
-async def query(url, query):
+async def query_with_url(url, query):
     model = OpenAIChatCompletionClient(
         model="o4-mini", 
         api_key=os.getenv(api_key_name),
-        temperature=0
+        temperature=1.0
     )
     agent = AssistantAgent(
         name="YouTube_Video_Proxy",
@@ -46,19 +46,21 @@ async def query(url, query):
         You are a helpful assistant, you will be given a url of a youtube video, and the user
         will ask you questions about the video. You can the tool to answer the user's questions. If 
         the answer to the question is not in the video, you should say "I don't know the answer." """,
+        reflect_on_tool_use=True,
         tools = [
              get_video_transcript,
              get_video_transcript_with_timestamp
-        ]
-        model = model
+        ],
+        model_client = model
     )
     
     task = f"This is the url of the youtube video: {url}. {query}"
     async for msg in agent.on_messages_stream([TextMessage(source='user', content=task)], 
                                               cancellation_token=CancellationToken()):
-        print(f"Agent: {msg.content}")
+        print(f"Agent: {msg}")
 
 if __name__ == "__main__":
     url = 'https://www.youtube.com/watch?v=9_PepvnqIfU&t=461s'
-    result = asyncio.run(get_video_transcript_with_timestamp(url))
+    query = "Summarize the video in a few sentences."
+    result = asyncio.run(query_with_url(url, query))
     print(result)
